82680415470c9690403b4ee1b7d2a94e0a40c4dd
SanghwaKo <sanghwako@gmail.com>
2017-11-26 17:20:15 +0100

    google-map applied.

--- app/src/main/java/com/gdghamburg/foodkart/MapActivity.java
+++ app/src/main/java/com/gdghamburg/foodkart/MapActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.gdghamburg.foodkart.model.Foodkarts;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,6 +15,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
@@ -69,129 +71,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
 
        // LatLngBounds.Builder builder = new LatLngBounds.Builder();
-
-        List<Food> foods = new List<Food>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @NonNull
-            @Override
-            public Iterator<Food> iterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @NonNull
-            @Override
-            public <T> T[] toArray(@NonNull T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(Food food) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(@NonNull Collection<? extends Food> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, @NonNull Collection<? extends Food> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Food get(int index) {
-                return null;
-            }
-
-            @Override
-            public Food set(int index, Food element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, Food element) {
-
-            }
-
-            @Override
-            public Food remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<Food> listIterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<Food> listIterator(int index) {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public List<Food> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
+        ArrayList<Food> foods = new ArrayList<>();
 
         Food food1 = new Food();
         food1.setPoint(new LatLng(53.551086, 9.993682));
@@ -201,8 +81,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         food2.setPoint(new LatLng(53.562546, 9.9598649));
         food2.setName("Eat together");
 
-        foods.add(food1);
-        foods.add(food2);
+        foods.add(0, food1);
+        foods.add(1, food2);
 
         for(int i=0; i<foods.size(); i++){
             String name = foods.get(i).getName();
